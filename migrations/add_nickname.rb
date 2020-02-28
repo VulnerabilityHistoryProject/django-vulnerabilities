910435b1a050b14318bf9006def95971ac368f6a
Andy Meneely <andy@se.rit.edu>
2020-02-28 16:28:30 -0500

    migrate to add nickname

--- migrations/add_nickname.rb
+++ migrations/add_nickname.rb
@@ -1,9 +1,11 @@
 require 'yaml'
 
+# MIGRATION STATUS: Done. No longer need to run this. Kept for posterity
+
 Dir['cves/*.yml'].each do |yml_file|
 
     h = YAML.load(File.open(yml_file, 'r').read)
-    h['nickname'] = ''
+    h['nickname'] = nil
     h['nickname_instructions'] = <<~EOS
       A catchy name for this vulnerability that would draw attention it. If the
       report mentions a nickname, use that. Must be under 30 characters.
@@ -11,6 +13,10 @@ Dir['cves/*.yml'].each do |yml_file|
     EOS
     File.open(yml_file, "w+") do |file|
       yml_txt = h.to_yaml[4..-1] # strip off ---\n
-      file.write(yml_txt)
+      stripped_yml = ""
+      yml_txt.each_line do |line|
+        stripped_yml += "#{line.rstrip}\n" # strip trailing whitespace
+      end
+      file.write(stripped_yml)
     end
 end
