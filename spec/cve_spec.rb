f19b90f37c67771f3fb22eb8fbc1335462ac3a12
Andy Meneely <andy@se.rit.edu>
2020-03-03 15:40:54 -0500

    new specs!

--- spec/cve_spec.rb
+++ spec/cve_spec.rb
@@ -36,31 +36,70 @@ describe 'CVE yml file' do
           end
         end
 
-        it 'have answers for description, unit_tested, discovered, subsystem filled out' do
+        it 'have a description and mistakes made written' do
           if(vuln['curated'])
             expect(vuln['description'].to_s).not_to be_empty
-            expect(vuln['unit_tested']['answer'].to_s).not_to be_empty
-            expect(vuln['discovered']['answer'].to_s).not_to be_empty
-            expect(vuln['subsystem']['answer'].to_s).not_to be_empty
             expect(vuln['mistakes']['answer'].to_s).not_to be_empty
+          end
+        end
+
+        it 'have answers for unit_tested questions' do
+          if(vuln['curated'])
+            expect(vuln['unit_tested']['code']).to be(true).or(be(false))
+            expect(vuln['unit_tested']['code_answer'].to_s).not_to be_empty
+            expect(vuln['unit_tested']['fix']).to be(true).or(be(false))
+            expect(vuln['unit_tested']['fix_answer'].to_s).not_to be_empty
+          end
+        end
+
+        it 'have answers discovered and discoverable' do
+          if(vuln['curated'])
+            expect(vuln['discovered']['answer'].to_s).not_to be_empty
+            expect(vuln['discovered']['automated']).to be(true).or(be(false))
+            expect(vuln['discovered']['contest']).to be(true).or(be(false))
+            expect(vuln['discovered']['developer']).to be(true).or(be(false))
+            expect(vuln['discoverable']['answer_note'].to_s).not_to be_empty
+            expect(vuln['discoverable']['answer']).to be(true).or(be(false))
+          end
+        end
+
+        it 'have answers for specification' do
+          if(vuln['curated'])
+            expect(vuln['specification']['answer_note'].to_s).not_to be_empty
+            expect(vuln['specification']['answer']).to be(true).or(be(false))
+          end
+        end
+
+        it 'have properly formatted subsystem names' do
+          if(vuln['curated'])
+            expect(vuln['subsystem']['answer'].to_s).not_to be_empty
+
+            subsystem_str = Array[vuln['subsystem']['name']].join
+            expect(subsystem_str).to match(/^[a-zA-Z\s0-9\_\-\@]+$/)
+          end
+        end
+
+        it 'have answers for i18n' do
+          if(vuln['curated'])
+            expect(vuln['i18n']['note'].to_s).not_to be_empty
+            expect(vuln['i18n']['answer']).to be(true).or(be(false))
+          end
+        end
+
+        it 'have answers for ipc' do
+          if(vuln['curated'])
+            expect(vuln['ipc']['note'].to_s).not_to be_empty
+            expect(vuln['ipc']['answer']).to be(true).or(be(false))
           end
         end
 
       end
 
-      it 'has true, false, or nil in various places' do
-        expect(vuln['unit_tested']['code']).to be(true).
-                                             or(be(false)).
-                                             or(be_nil)
-        expect(vuln['unit_tested']['fix']).to be(true).
-                                           or(be(false)).
-                                           or(be_nil)
-        expect(vuln['discovered']['automated']).to be(true).
-                                                or(be(false)).
-                                                or(be_nil)
-        expect(vuln['discovered']['google']).to be(true).
-                                                or(be(false)).
-                                                or(be_nil)
+      it 'has an empty nickname or under 30 chars' do
+        expect(vuln['nickname'].to_s.length).to be <= 30
+      end
+
+      it 'has lessons properly formatted' do
         expect(vuln['lessons']['defense_in_depth']['applies']).to be(true).
                                                                or(be(false)).
                                                                or(be_nil)
