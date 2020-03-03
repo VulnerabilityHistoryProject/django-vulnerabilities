95d016693da6539e9a5519ba9e36a26560f199b5
Andy Meneely <andy@se.rit.edu>
2020-03-03 12:00:08 -0500

    finishing touches

--- migrations/2020_03_03_add_automated.rb
+++ migrations/2020_03_03_add_automated.rb
@@ -41,8 +41,8 @@ def order_of_keys
     CWE_instructions
     CWE
     CWE_note
-    nickname
     nickname_instructions
+    nickname
   )
 end
 
@@ -55,23 +55,25 @@ For one-line strings, you can just use quotes after the colon
 For multi-line strings, as we do for our instructions, you put a | and then
 indent by two spaces
 
-For readability, we hard-wrap multi-line strings at 80 characters. This is not absolutely required, but appreciated.
+For readability, we hard-wrap multi-line strings at 80 characters. This is
+not absolutely required, but appreciated.
   EOS
 end
 
 def discoverable_question
   instructions = <<~EOS
-Is it plausible that a fully automated tool could have discovered this? These
-are tools that require little knowledge of the domain, e.g. automatic static
-analysis, compiler warnings, fuzzers.
+Is it plausible that a fully automated tool could have discovered
+this? These are tools that require little knowledge of the domain,
+ e.g. automatic static analysis, compiler warnings, fuzzers.
 
 Examples for true answers: SQL injection, XSS, buffer overflow
 
-Examples for false: RFC violations, permissions issues, anything that
-    requires the tool to be "aware" of the project's domain-specific
-    requirements.
+Examples for false: RFC violations, permissions issues, anything
+that requires the tool to be "aware" of the project's
+domain-specific requirements.
 
-The answer field should be boolean. In answer_note, please explain why you come to that conclusion.
+The answer field should be boolean. In answer_note, please explain
+why you come to that conclusion.
   EOS
   return {
     'instructions' => instructions,
@@ -86,9 +88,11 @@ Is there mention of a violation of a specification? For example,
 an RFC specification, a protocol specification, or a requirements
 specification.
 
-Be sure to check all artifacts for this: bug report, security advisory, commit message, etc.
+Be sure to check all artifacts for this: bug report, security
+advisory, commit message, etc.
 
-The answer field should be boolean. In answer_note, please explain why you come to that conclusion.
+The answer field should be boolean. In answer_note, please explain
+why you come to that conclusion.
   EOS
   return {
     'instructions' => specification_instructions,
@@ -97,21 +101,53 @@ The answer field should be boolean. In answer_note, please explain why you come 
   }
 end
 
+def cwe_inst
+  <<~EOS
+Please go to http://cwe.mitre.org and find the most specific, appropriate CWE
+entry that describes your vulnerability. We recommend going to
+https://cwe.mitre.org/data/definitions/699.html for the Software Development
+view of the vulnerabilities. We also recommend the tool
+http://www.cwevis.org/viz to help see how the classifications work.
 
-Dir['cves/*.yml'].each do |yml_file|
+If you have anything to note about why you classified it this way, write
+something in CWE_note. This field is optional.
+
+Just the number here is fine. No need for name or CWE prefix. If more than one
+apply here, then choose the best one and mention the others in CWE_note.
+  EOS
+end
+
+def i18n_question
+  <<~EOS
+  Was the feature impacted by this vulnerability about internationalization
+  (i18n)? An internationalization feature is one that enables people from all
+  over the world to use the system. This includes translations, locales,
+  typography, unicode, or various other features.
+
+  Answer should be boolean. Write a note about how you came to the conclusions
+  you did.
+  EOS
+end
+
+
+
+ymls = Dir['cves/*.yml'] + ['skeletons/cve.yml']
+ymls.each do |yml_file|
     h = YAML.load(File.open(yml_file, 'r').read)
     h['yaml_instructions'] = updated_yaml_instructions
     h['discoverable'] = discoverable_question
     h['specification'] = spec_question
+    h['CWE_instructions'] = cwe_inst
+    h['i18n']['question'] = i18n_question
     # Reconstruct the hash in the order we specify
     out_h = {}
     order_of_keys.each do |key|
-      out_h = h[key]
+      out_h[key] = h[key]
     end
 
     # Generate the new YML, clean it up, write it out.
     File.open(yml_file, "w+") do |file|
-      yml_txt = h.to_yaml[4..-1] # strip off ---\n
+      yml_txt = out_h.to_yaml[4..-1] # strip off ---\n
       stripped_yml = ""
       yml_txt.each_line do |line|
         stripped_yml += "#{line.rstrip}\n" # strip trailing whitespace
